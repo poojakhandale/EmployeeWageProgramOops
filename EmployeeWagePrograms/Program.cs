90210de42a2495f5e78af0e3b912a9bedac10031
poojakhandale <poojakhandale2000@gmail.com>
2023-02-20 22:01:36 +0530

    Greeting massage

---
+++ EmployeeWagePrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EmployeeWageComputationUsingOOPS
+{
+    public class EmployeeWage
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Computation");
+        }
+    }
+}
+     
